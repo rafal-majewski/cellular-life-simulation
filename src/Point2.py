5d1899730a9125fd2721c56521c54abf517f46d8
Rafał Majewski <goodheropl@gmail.com>
2022-04-14 16:48:48 +0200

    point magic methods

--- src/Point2.py
+++ src/Point2.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Any
 
 
@@ -24,3 +25,15 @@ class Point2:
 		if not isinstance(other, Point2):
 			return False
 		return self.x == other.x and self.y == other.y
+
+	def __add__(self, other: Point2) -> Point2:
+		return Point2(self.x + other.x, self.y + other.y)
+
+	def __sub__(self, other: Point2) -> Point2:
+		return Point2(self.x - other.x, self.y - other.y)
+
+	def __mul__(self, scalar: float) -> Point2:
+		return Point2(self.x * scalar, self.y * scalar)
+
+	def __truediv__(self, scalar: float) -> Point2:
+		return Point2(self.x / scalar, self.y / scalar)
