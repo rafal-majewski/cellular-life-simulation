0e2c4f6b1f5bd060f90c30589dbd6c90c925168e
Rafał Majewski <goodheropl@gmail.com>
2022-04-14 23:48:36 +0200

    add example cell

--- src/__main__.py
+++ src/__main__.py
@@ -3,8 +3,9 @@ from src.game.Game import Game
 from src.pygameimp.Camera import Camera
 from src.abstractcamera.AbstractCamera import AbstractCamera
 from src.engine.World import World
-import pygame
 from src.pygameimp.Drawer import Drawer
+from src.engine.Cell import Cell
+from src.engine.Point2 import Point2
 
 
 if __name__ == "__main__":
@@ -12,6 +13,7 @@ if __name__ == "__main__":
 	surface: pygame.surface.Surface = pygame.display.set_mode((800, 600))
 	drawer: Drawer = Drawer(pygame.display.set_mode((800, 600)))
 	world: World = World()
+	world.addCell(Cell(position=Point2(0, 0), mass=1, radius=10))
 	camera: AbstractCamera = Camera(drawer)
 	game: Game = Game(world=world, camera=camera)
 	game.start()
