b5c1c406d0cef97dd4174644915fa8c797f0aff4
Rafał Majewski <goodheropl@gmail.com>
2022-04-26 13:00:30 +0200

    temp world borders

--- src/engine/physics/MovementResolver.py
+++ src/engine/physics/MovementResolver.py
@@ -1,4 +1,5 @@
 from src.engine.physics.CellView import CellView
+from src.utils.point2.FloatPoint2 import FloatPoint2
 import math
 
 
@@ -8,3 +9,13 @@ class MovementResolver:
 		# parameterize the class
 		cellView.position += cellView.velocity * deltaTime
 		cellView.velocity *= math.pow(0.95, deltaTime)
+		# todo to remove:
+		# temp:
+		if cellView.position.x > 400.0:
+			cellView.velocity = FloatPoint2(-abs(cellView.velocity.x), cellView.velocity.y)
+		if cellView.position.x < -400:
+			cellView.velocity = FloatPoint2(abs(cellView.velocity.x), cellView.velocity.y)
+		if cellView.position.y > 300.0:
+			cellView.velocity = FloatPoint2(cellView.velocity.x, -abs(cellView.velocity.y))
+		if cellView.position.y < -300:
+			cellView.velocity = FloatPoint2(cellView.velocity.x, abs(cellView.velocity.y))
